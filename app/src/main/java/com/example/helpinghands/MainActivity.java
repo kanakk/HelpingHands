a186659decba3e19145f7ccdc63820e7bb0cd40a
kanakk <kanakkawadiwale@gmail.com>
2019-03-16 13:36:33 +0530

    Added StudentRegistrationGUI (By Reshma) Added Database authentication

--- app/src/main/java/com/example/helpinghands/MainActivity.java
+++ app/src/main/java/com/example/helpinghands/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.helpinghands;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+    }
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser==null)//The user has not signed in
+        {
+            sendToRegistration();
+        }
+    }
+
+    private void sendToRegistration()
+    {
+        Intent startIntent=new Intent(MainActivity.this,RegistrationActivity.class);
+        startActivity(startIntent);
+        finish();//to disable the back button
     }
 }
